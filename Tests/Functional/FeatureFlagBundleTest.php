55a9c780850b559569370c04998f6a32a0771635
Matthias Molitor <mmo@webfactory.de>
2016-05-23 01:41:27 +0200

    added test stubs

---
+++ Tests/Functional/FeatureFlagBundleTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Matthimatiker\FeatureFlagBundle\Tests\Functional;
+
+class FeatureFlagBundleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGrantsAccessToAssignedFeature()
+    {
+
+    }
+
+    public function testDeniesAccessToNotAssignedFeature()
+    {
+
+    }
+
+    public function testDeniesAccessToNotExistingFeature()
+    {
+
+    }
+
+    public function testGrantsAccessToFeaturesInheritedFromAnotherFeature()
+    {
+
+    }
+
+    public function testGrantsAccessToFeatureInheritedFromAnotherRole()
+    {
+
+    }
+
+    public function testGrantsAccessToRoleInheritedFromAnotherRole()
+    {
+
+    }
+}
