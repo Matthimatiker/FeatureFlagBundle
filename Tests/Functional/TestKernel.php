f33b1cbe1dfdd512ffb02453f4ea35e85029f148
Matthias Molitor <mmo@webfactory.de>
2016-06-01 01:49:16 +0200

    configure a minimal kernel to test the functionality

---
+++ Tests/Functional/TestKernel.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Matthimatiker\FeatureFlagBundle\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
+
+/**
+ * Minimal kernel for testing.
+ */
+class TestKernel extends Kernel
+{
+    use MicroKernelTrait;
+
+    /**
+     * Returns an array of bundles to register.
+     *
+     * @return BundleInterface[] An array of bundle instances.
+     */
+    public function registerBundles()
+    {
+        return array();
+    }
+
+    /**
+     * Add or import routes into your application.
+     *
+     *     $routes->import('config/routing.yml');
+     *     $routes->add('/admin', 'AppBundle:Admin:dashboard', 'admin_dashboard');
+     *
+     * @param RouteCollectionBuilder $routes
+     */
+    protected function configureRoutes(RouteCollectionBuilder $routes)
+    {
+    }
+
+    /**
+     * Configures the container.
+     *
+     * You can register extensions:
+     *
+     * $c->loadFromExtension('framework', array(
+     *     'secret' => '%secret%'
+     * ));
+     *
+     * Or services:
+     *
+     * $c->register('halloween', 'FooBundle\HalloweenProvider');
+     *
+     * Or parameters:
+     *
+     * $c->setParameter('halloween', 'lot of fun');
+     *
+     * @param ContainerBuilder $c
+     * @param LoaderInterface $loader
+     */
+    protected function configureContainer(ContainerBuilder $c, LoaderInterface $loader)
+    {
+
+    }
+}
