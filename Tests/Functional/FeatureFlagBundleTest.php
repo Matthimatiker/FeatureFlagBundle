1a521ff1304678de1b332ea199b6f9aede3a3f9e
Matthias Molitor <mmo@webfactory.de>
2016-06-01 03:02:32 +0200

    create and boot kernel in test

--- Tests/Functional/FeatureFlagBundleTest.php
+++ Tests/Functional/FeatureFlagBundleTest.php
@@ -4,6 +4,31 @@ namespace Matthimatiker\FeatureFlagBundle\Tests\Functional;
 
 class FeatureFlagBundleTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var TestKernel
+     */
+    private $kernel = null;
+
+    /**
+     * Initializes the test environment.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->kernel = new TestKernel();
+        $this->kernel->boot();
+    }
+
+    /**
+     * Cleans up the test environment.
+     */
+    protected function tearDown()
+    {
+        $this->kernel->shutdown();
+        $this->kernel = null;
+        parent::tearDown();
+    }
+
     public function testGrantsAccessToAssignedFeature()
     {
 
